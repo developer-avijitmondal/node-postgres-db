f1f1164d4730e9f190701d5947535b0132d04d3a
developer-avijitmondal <developer.avijitmondal@gmail.com>
2020-08-18 13:00:32 +0530

    docker changes

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-if [ "$NODE_ENV" == "production" ] ; then
-  npm run start
-else
-#   npm run dev
-  npm run start
-fi
+# if [ "$NODE_ENV" == "production" ] ; then
+#   npm run start
+# else
+# #   npm run dev
+#   npm run start
+# fi
+
+# npx sequelize-cli db:migrate
+
+npm start
