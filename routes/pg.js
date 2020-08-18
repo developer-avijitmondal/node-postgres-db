18aa588a4151a1e5e895fcef692aba0ce40f4126
developer-avijitmondal <developer.avijitmondal@gmail.com>
2020-08-18 13:30:57 +0530

    pg changed

--- routes/pg.js
+++ routes/pg.js
@@ -1,13 +1,25 @@
 const { Pool, Client } = require('pg');
 require('dotenv').config();
 
+// "username": process.env.DB_USER,
+// "password": process.env.DB_PASS,
+// "database": process.env.DB_NAME,
+// "host": process.env.DB_HOST,
+
+// const client = new Client({ //
+//     //database: process.env.DB_NAME,
+//     // database: "node_test_db",
+//     // password: "postgres",
+//     // user: "postgres",
+//     // host: "postgres",
+//     //port: 9229
+// });
+
 const client = new Client({ //
-    //database: process.env.DB_NAME,
-    database: "node_test_db",
-    password: "postgres",
-    user: "postgres",
-    host: "postgres",
-    //port: 9229
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASS,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST
 });
 
 // const client = new Client({
