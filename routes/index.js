de6c2206516d50598dd672edc141bd13e7fb32dc
developer-avijitmondal <developer.avijitmondal@gmail.com>
2020-08-18 13:39:48 +0530

    check sequelize

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 // const client = require("./pg");
+const db = require("../models");
 
 /* GET home page. */
 router.get('/', function(req, res, next) { //
