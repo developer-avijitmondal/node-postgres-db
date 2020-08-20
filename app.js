d853d6b672ce3a10aaba425d08cf9e10e23c4f98
developer-avijitmondal <developer.avijitmondal@gmail.com>
2020-08-20 19:03:52 +0530

    crud in postgres docker

--- app.js
+++ app.js
@@ -1,42 +1,39 @@
 var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
+var path = require('path');//path
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var debug = require('debug')('node-postgres:server');
+var debug = require('debug')('poneabackendv1.0.0:server');
 var http = require('http');
-const client = require("./routes/pg");
 const bodyParser =  require('body-parser');
 const passport = require('passport');
 const cors = require('cors');
-
+require('dotenv').config();
+const swaggerUi = require('swagger-ui-express');
+const pg = require('./routes/pg');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var swaggerUiRoute = require('./routes/swagger');
 var businessUnitGroups = require('./routes/businessunitgroups');
+
 var app = express();
 
-// view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
 
 app.use(logger('dev'));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.urlencoded({ extended:true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors());
 app.use(passport.initialize());
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', swaggerUiRoute);
-app.use('/index', indexRouter);
+app.use('/', indexRouter);
 app.use('/users', usersRouter);
+// app.use('/business', businessRouter);
+app.use('/swagger', swaggerUiRoute);
+// app.use('/franchises', franchisesRouter);
 
 app.use('/api/v1/businessunitgroups', businessUnitGroups);
-// app.use('/swagger', swaggerUiRoute);
+
 
 // headers and content type
 app.use(function (req, res, next) {
@@ -44,22 +41,6 @@ app.use(function (req, res, next) {
   //res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
-});
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
 });
 
 /**
